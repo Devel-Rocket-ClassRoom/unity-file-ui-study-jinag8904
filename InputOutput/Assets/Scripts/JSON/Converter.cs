66547ead41475d789cdd74fc32111199f2a0f5e3
jinag8904 <jinag8904@gmail.com>
2026-04-15 16:27:02 +0900

    게임 오브젝트 저장/로드하기

--- InputOutput/Assets/Scripts/JSON/Converter.cs
+++ InputOutput/Assets/Scripts/JSON/Converter.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class Vector3Converter: JsonConverter<Vector3>
 {
--- InputOutput/Assets/Scripts/JSON/SilSoup1.cs
+++ InputOutput/Assets/Scripts/JSON/SilSoup1.cs
@@ -1,21 +1,29 @@
-using System;
-using System.IO;
-using UnityEngine;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
 
 [Serializable]
 public class SomeClass
 {
-    [JsonConverter(typeof(Vector3Converter))]
     public Vector3 pos;
-
-    [JsonConverter(typeof(QuaternionConverter))]
     public Quaternion rot;
-
-    [JsonConverter(typeof(Vector3Converter))]
     public Vector3 scale;
+    public Color color;
 
-    [JsonConverter(typeof(ColorConverter))]
+    public override string ToString() => $"{pos} / {rot} / {scale} / {color}";
+}
+
+[Serializable]
+public class ObjectSave
+{
+    public PrimitiveType type;
+    public Vector3 pos;
+    public Quaternion rot;
+    public Vector3 scale;
     public Color color;
 
     public override string ToString() => $"{pos} / {rot} / {scale} / {color}";
@@ -23,7 +31,7 @@ public class SomeClass
 
 public class SilSoup1 : MonoBehaviour
 {
-    public GameObject cube;
+    public List<GameObject> objects;
 
     public string fileName = "test.json";
     public string FileFullPath => Path.Combine(Application.persistentDataPath, "JsonTest", fileName);
@@ -40,30 +48,69 @@ public class SilSoup1 : MonoBehaviour
 
     public void Save()
     {
-        var obj = new SomeClass()
-        {
-            pos = cube.transform.position,
-            rot = cube.transform.rotation,
-            scale = cube.transform.localScale,
-            color = cube.GetComponent<Renderer>().material.color
-        };
+        var saveObjs = new List<ObjectSave>();
 
-        var json = JsonConvert.SerializeObject(obj, jsonSettings);
-        
+        for (int i = 0; i < objects.Count; i++)
+        {
+            var typeName = objects[i].GetComponent<MeshFilter>().sharedMesh.name;
+            bool saveType = Enum.TryParse<PrimitiveType>(typeName, true, out PrimitiveType result);
+
+            var saveObj = new ObjectSave()
+            {
+                type = result,
+                pos = objects[i].transform.position,
+                rot = objects[i].transform.rotation,
+                scale = objects[i].transform.localScale,
+                color = objects[i].GetComponent<Renderer>().material.color
+            };
+            
+            saveObjs.Add(saveObj);
+        }
+
+        var json = JsonConvert.SerializeObject(saveObjs, jsonSettings);
         File.WriteAllText(FileFullPath, json);
-        Debug.Log($"저장: {json}");
     }
 
     public void Load()
     {
-        var json = File.ReadAllText(FileFullPath);
-        var obj = JsonConvert.DeserializeObject<SomeClass>(json, jsonSettings);
+        Clear();
 
-        cube.transform.position = obj.pos;
-        cube.transform.rotation = obj.rot;
-        cube.transform.localScale = obj.scale;
-        cube.GetComponent<Renderer>().material.color = obj.color;
+        var jsons = File.ReadAllText(FileFullPath);
+        var loadObj = JsonConvert.DeserializeObject<List<ObjectSave>>(jsons, jsonSettings);
 
-        Debug.Log($"로드: {json}");
+        for (int i = 0; i < loadObj.Count; i++)
+        {
+            var typeName = loadObj[i].type;
+
+            var obj = GameObject.CreatePrimitive(typeName);
+            obj.transform.position = loadObj[i].pos;
+            obj.transform.rotation = loadObj[i].rot;
+            obj.transform.localScale = loadObj[i].scale;
+            obj.GetComponent<Renderer>().material.color = loadObj[i].color;
+                        
+            objects.Add(obj);
+        }
+
+        Debug.Log($"로드: {objects.Count}개");
+    }
+
+    public void Create()
+    {
+        System.Random rand = new();
+        var newObj = GameObject.CreatePrimitive((PrimitiveType)rand.Next(0, 4));
+        newObj.transform.position = new Vector3(rand.Next(-10, 11), rand.Next(-3, 4), transform.position.z);
+
+        objects.Add(newObj);
+    }
+
+    public void Clear()
+    {
+        foreach (var obj in objects)
+        {
+            Debug.Log($"삭제: {obj.name}");
+            Destroy(obj);
+        }
+
+        objects.Clear();
     }
 }
